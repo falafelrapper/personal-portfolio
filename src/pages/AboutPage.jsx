8be39e582d08d407867f46d410abcff7dd85bae9
Zachary Roy <vgd199@gmail.com>
2024-03-01 21:12:23 -0800

    reworked my resume page, it will now render better when loaded, and is currently using my current template resume

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -10,18 +10,35 @@ export default function AboutPage() {
     const combinedFiles = [...imageUrls, pdfFile];
     preloadFiles(combinedFiles);
   }, []);
-  
+
   return (
     <div className="about-me">
       <img id='zach-pic' src="/content/about-me.png" alt="Zachary Roy" />
       <h2>About Me</h2>
       <p>
-        I am a high school graduate currently currently enrolled in UCLA edX's Full Stack 
-        Development Boot Camp who is passionate about upcoming technologies and what 
-        computers can do as an aid to society. When I am not programming or studying computer
-        science, you can catch me playing drums for acts such as Meishi Smile or Pacific Palace.
-        Working together, we can get your next application deployed and ready for the world to see.
+        I'm a burgeoning full stack web developer currently
+        expanding my horizons through UCLA's edX Full Stack Development Boot Camp. Based in
+        the vibrant city of Los Angeles, CA, I'm deeply immersed in the world of coding, constantly
+        exploring the vast potential of upcoming technologies and their capacity
+        to contribute positively to society. My journey into the realm of technology started with 
+        a simple curiosity about how computers work and what they can achieve. This curiosity 
+        quickly evolved into a passion, leading me to where I am today – on the cusp of 
+        completing an intensive program at UCLA designed to arm me with the skills needed to build 
+        comprehensive, user-friendly, and efficient web applications from scratch.
+        When I'm not elbows-deep in code, I express myself through music. I'm a drummer for bands 
+        such as Meishi Smile and Pacific Palace, where I find a different kind of rhythm and 
+        collaboration that parallels the teamwork and creativity I bring to web development projects.
       </p>
+      <h4>My Mission</h4>
+      <p>I believe in the power of collaboration to bring innovative ideas to life. Whether
+        it's developing the next groundbreaking application or creating digital solutions that
+        make a difference, I'm here to turn visions into reality. With a blend of technical
+        knowledge, creative problem-solving, and a keen eye for design, I'm ready to tackle
+        challenges and contribute to projects that aim for impact and excellence. If you're 
+        looking for a dedicated and creative full stack developer with a unique
+        perspective, let's get in touch. Together, we can deploy applications that not only
+        meet user needs but also exceed expectations, making a mark on the digital landscape.</p>
+
       <p>All references are available upon request.</p>
     </div>
   );
