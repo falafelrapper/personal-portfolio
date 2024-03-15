73aeb4d6ec5dc1418f775bc47c48c95e6d3d6f6b
Zachary Roy <vgd199@gmail.com>
2024-03-14 22:50:56 -0700

    fixed some spacing with portfolio cards, fixed sizing so it's more legible, made some fonts more static, will only change size when shrunk to mobile now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom';
-import Nav from './components/Nav';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css'
 
 function App() {
   return (
     <>
-      <Nav />
+      <Header />
       <Outlet />
       <Footer />
     </>
