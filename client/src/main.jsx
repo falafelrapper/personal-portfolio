cf8cab2363ed266645cd01fed0bebfc6c8ca241e
Zachary Roy <vgd199@gmail.com>
2024-04-11 13:19:29 -0700

    added SCSS, changed animations from padding/margin to transform/translate, and then made animation delays on the project cards

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import './App.css';
+import './App.scss';
 
 
 import App from './App';
